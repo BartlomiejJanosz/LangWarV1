8c31dc6c06173fcbce7b5c936bb926f1ca3d5953
Bartłomiej Janosz <bartlomiej.janosz@gmail.com>
2019-09-01 21:20:40 +0200

    Tests of server

--- LangWarV1/TesttForm.cs
+++ LangWarV1/TesttForm.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Web.Script.Serialization;
 
 namespace LangWarV1
 {
@@ -24,13 +25,13 @@ namespace LangWarV1
 
         private async void AddBattleInfoToTableButton_Click(object sender, EventArgs e)
         {
-            var firstCard = new Card(1, 3, "pies", "book", "PL");
-            var secondCard = new Card(2, 4, "kot", "library", "ANG");
+            var firstCard = new Card(1, 5, "słoń", "elephant", "PL");
+            var secondCard = new Card(2, 1, "library", "biblioteka", "ANG");
 
-            var battleInfo = new BattleInfo(1, 1, 30, firstCard, secondCard);
+            var battleInfo = new BattleInfo(-1, 1, 30, firstCard, secondCard);
 
-            var stringBattleInfo = await Task.Run(() => JsonConvert.SerializeObject(battleInfo));
-            var httpContent = new StringContent(stringBattleInfo, Encoding.UTF8, "application/json");
+            var battleInfoAsString = await Task.Run(() => JsonConvert.SerializeObject(battleInfo));
+            var httpContent = new StringContent(battleInfoAsString, Encoding.UTF8, "application/json");
 
             using (HttpClient httpClient = new HttpClient())
             {
@@ -43,8 +44,19 @@ namespace LangWarV1
             using (HttpClient httpClient = new HttpClient())
             {
                 var httpResponse = await httpClient.GetAsync(_baseAddress + "api/values/battleInfo/1");
-                var resultasstring = httpResponse.Content.ReadAsStringAsync().Result;
+                var resultAsString = httpResponse.Content.ReadAsStringAsync().Result;
+                //Do not know why but string that comes from server is modified and has '\'added in some places plus '"' at the end and the beginning
+                var resultReadyToBeDeserialized = PrepareStringToDeserialization(resultAsString);
+                BattleInfo battleInfo = await Task.Run(() => JsonConvert.DeserializeObject<BattleInfo>(resultReadyToBeDeserialized));
             }
+        }
+
+        private static string PrepareStringToDeserialization(string resultasstring)
+        {
+            resultasstring = resultasstring.Replace(@"\", "");
+            resultasstring = resultasstring.Substring(1);
+            resultasstring = resultasstring.Remove(resultasstring.Length - 1);
+            return resultasstring;
         }
     }
 }
